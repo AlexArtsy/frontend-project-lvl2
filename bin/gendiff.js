d093431f6bc86b5b10f22d46a5d2540d0e4cb0b6
Alex Arts <a_arz@mail.ru>
2022-07-27 17:13:32 +0400

    fix problem with relative paths

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (filePath1, filePath2) => {
+  const contentFile1 = fs.readFileSync(path.resolve(process.cwd(), filePath1), 'utf-8');
+  const contentFile2 = fs.readFileSync(path.resolve(process.cwd(), filePath2), 'utf-8');
+
+  console.log('file1:', contentFile1);
+  console.log('file2:', contentFile2);
+};
 
 const program = new Command();
 
 program
   .name('gendiff')
   .usage('gendiff [options] <filepath1> <filepath2>')
+  .argument('<filePath1>', 'path to file1')
+  .argument('<filePath2>', 'path to file2')
+  .option('-f, --format <type>', 'output format')
+  .action((filePath1, filePath2) => {
+    genDiff(filePath1, filePath2);
+  })
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format');
-  //.option('-h, --help', 'output usage information');
+  .option('-h, --help', 'output usage information');
 
 program.parse();
+
+const options = program.opts();
+console.log('format type:', options.format);
+
+export default genDiff;
