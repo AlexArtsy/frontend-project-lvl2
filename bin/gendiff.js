c682cf2216bd82c14397f742f5c60a128c1c39b5
Alex Arts <a_arz@mail.ru>
2022-07-27 12:35:24 +0400

    Step 2 done and add file1.json, file2.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,11 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .usage('gendiff [options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --version', 'output the version number');
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
+  //.option('-h, --help', 'output usage information');
 
 program.parse();
