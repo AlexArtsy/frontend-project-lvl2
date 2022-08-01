ea843ef9b6cdff8dfe583139c8b051da720bbf7f
Alex Arts <a_arz@mail.ru>
2022-08-01 13:05:23 +0400

    make sort function and change project structure: add index.js (with common logic), change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,34 +1,25 @@
 #!/usr/bin/env node
+//  это исполняемый файл - утилита (не путать с модулем src/gendiff.js)
 import { Command } from 'commander';
-import fs from 'fs';
-import path from 'path';
-
-const genDiff = (filePath1, filePath2) => {
-  const contentFile1 = fs.readFileSync(path.resolve(process.cwd(), filePath1), 'utf-8');
-  const contentFile2 = fs.readFileSync(path.resolve(process.cwd(), filePath2), 'utf-8');
-
-  console.log('file1:', contentFile1);
-  console.log('file2:', contentFile2);
-};
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .usage('gendiff [options] <filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the version number')
   .argument('<filePath1>', 'path to file1')
   .argument('<filePath2>', 'path to file2')
   .option('-f, --format <type>', 'output format')
   .action((filePath1, filePath2) => {
-    genDiff(filePath1, filePath2);
-  })
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --version', 'output the version number')
-  .option('-h, --help', 'output usage information');
+    console.log(genDiff(filePath1, filePath2));
+  });
 
 program.parse();
 
 const options = program.opts();
-console.log('format type:', options.format);
+//console.log('format type:', options.format);
 
 export default genDiff;
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (filePath1, filePath2) => {
+  const getObjectfromPath = (value) => {
+    const content = fs.readFileSync(path.resolve(process.cwd(), value), 'utf-8');
+    return JSON.parse(content);
+  };
+
+  const sortObjectKyes = (obj) => {
+    const ordered = {};
+    Object.keys(obj).sort().forEach((key) => {
+      ordered[key] = obj[key];
+    });
+    return ordered;
+  };
+
+  const sortEntries = (arr) => {
+    const ordered = arr.sort((item1, item2) => {
+      const [a1, b1, c1] = item1;
+      const [a2, b2, c2] = item2;
+      if (b1 < b2) {
+        return -1;
+      }
+      if (b1 > b2) {
+        return 1;
+      }
+      return 0;
+    });
+
+    return ordered;
+  };
+
+  const checkDiff = (item1, item2) => {
+    const file1CommonEntries = item1.reduce((acc, entry) => {
+      const [key1, value1] = entry;
+      let stat = false;
+      for (let i = 0; i < item2.length; i += 1) {
+        const [key2, value2] = item2[i];
+        if (key1 === key2 && value1 === value2) {
+          stat = true;
+          item2.splice(i, 1);
+          acc.push([' ', key1, value1]);
+        }
+        if (key1 === key2 && value1 !== value2) {
+          stat = true;
+          acc.push(['-', key1, value1]);
+          acc.push(['+', key2, value2]);
+          item2.splice(i, 1);
+        }
+      }
+      if (!stat) {
+        acc.push(['-', key1, value1]);
+      }
+      return acc;
+    }, []);
+    const file2CommonEntries = item2.map((entry) => {
+      const [key, value] = entry;
+      return ['+', key, value];
+    });
+    return file1CommonEntries.concat(file2CommonEntries);
+  };
+
+  //const obj1entryes = Object.entries(sortObjectKyes(getObjectfromPath(filePath1)));
+  //const obj2entryes = Object.entries(sortObjectKyes(getObjectfromPath(filePath2)));
+  const obj1entryes = Object.entries(getObjectfromPath(filePath1));
+  const obj2entryes = Object.entries(getObjectfromPath(filePath2));
+
+  //const result = checkDiff(obj1entryes, obj2entryes);
+  const result = sortEntries(checkDiff(obj1entryes, obj2entryes));
+  return result;
+};
+
+export default genDiff;
