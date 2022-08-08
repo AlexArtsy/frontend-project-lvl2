65ea495a1f95af0aad87fd308c566f29a16c6e50
Alex Arts <a_arz@mail.ru>
2022-08-08 09:39:23 +0400

    fix some problems

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import genDiff from '../src/index.js';
+import genDiff from '../src/index';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,9 +13,18 @@ const readFile = (fileName) => fs.readFileSync(getPath(fileName), 'utf-8');
 test('test genDiff json', () => {
   const path1 = getPath('1.json');
   const path2 = getPath('2.json');
-  console.log(path1);
-  console.log(path2);
+  //  console.log(path1);
+  //  console.log(path2);
   const result = readFile('result1_2_json');
+  //  console.log(result);
 
-  expect('').toEqual('');
+  expect(result).toEqual(genDiff(path1, path2));
+});
+
+test('test genDiff faulty', () => {
+  const path1 = getPath('1.json');
+  const path2 = getPath('2.json');
+  const result = readFile('fault1_2_json');
+
+  expect(result).toEqual(genDiff(path1, path2));
 });
