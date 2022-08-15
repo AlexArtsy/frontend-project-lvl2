9e2352295073ab62577147a1fc555683d97acbbc
Alex Arts <a_arz@mail.ru>
2022-08-15 17:33:32 +0400

    add new tests: .yml abd .yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 const getPath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const readFile = (fileName) => fs.readFileSync(getPath(fileName), 'utf-8');
 
-test('test genDiff json', () => {
+test('test genDiff .json', () => {
   const path1 = getPath('1.json');
   const path2 = getPath('2.json');
   //  console.log(path1);
@@ -21,10 +21,18 @@ test('test genDiff json', () => {
   expect(result).toEqual(genDiff(path1, path2));
 });
 
-test('test genDiff faulty', () => {
-  const path1 = getPath('1.json');
-  const path2 = getPath('2.json');
-  const result = readFile('fault1_2_json');
+test('test genDiff .yml', () => {
+  const path1 = getPath('file1.yml');
+  const path2 = getPath('file2.yml');
+  const result = readFile('result1_2_json');
 
-  expect('').toEqual('');
+  expect(result).toEqual(genDiff(path1, path2));
+});
+
+test('test genDiff .yaml', () => {
+  const path1 = getPath('file1.yaml');
+  const path2 = getPath('file2.yaml');
+  const result = readFile('result1_2_json');
+
+  expect(result).toEqual(genDiff(path1, path2));
 });
