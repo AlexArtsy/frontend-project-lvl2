209706819ecf8d7cef4defee971ac41cb6a9dc91
Alex Arts <a_arz@mail.ru>
2022-09-01 09:47:20 +0400

    fix problem with tabulation in json formatter

--- formatters/json.js
+++ formatters/json.js
@@ -1,16 +1,33 @@
-const json = (data, spaceCount = 0) => {
-  const lb = '\n';
-  const tab = '  '.repeat(spaceCount);
-  const result = data.reduce((acc, [symb, key, value, pathName, plainData = ['', '', '']]) => {
-    const [stat, from, to] = plainData;
-    let newValue = value;
-    if (Array.isArray(value)) {
-      newValue = json(value, spaceCount + 1);
-    }
-    const mainData = `${tab}{${lb}"symbol": ${symb},${lb}${tab}"key": ${key},${lb}${tab}"value": ${newValue},${lb}${tab}"path": ${pathName},${lb}`;
-    const plainDt = `${tab}"plainData": {${lb}${tab}  "status": ${stat},${lb}${tab}  "from": ${from},${lb}${tab}  "to": ${to}${lb}${tab}}${lb}`;
-    return [...acc, `[${mainData}${plainDt}]`];
-  }, '');
-  return result;
+const lb = '\n'; // linebreak symbol
+const tb = '  '; // tab symbol
+
+const getTab = (count) => tb.repeat(count);
+
+const convertToJson = ([symb, key, value, pathName, plainData = ['', '', '']], spaceCount) => {
+  const tab = getTab(spaceCount);
+  const [stat, from, to] = plainData;
+  const finalFrom = Array.isArray(from) ? '[complex value]' : from;
+  const finalTo = Array.isArray(to) ? '[complex value]' : to;
+  const s = `${tab}"symbol": ${symb},${lb}`;
+  const k = `${tab}"key": ${key},${lb}`;
+  const v = `${tab}"value": ${value},${lb}`;
+  const pth = `${tab}"path": ${pathName},${lb}`;
+  const st = `${getTab(spaceCount + 1)}"status": ${stat},${lb}`;
+  const fr = `${getTab(spaceCount + 1)}"from": ${finalFrom},${lb}`;
+  const t = `${getTab(spaceCount + 1)}"to": ${finalTo}${lb}`;
+  const plD = `${tab}"plainData": {${lb}${st}${fr}${t}${tab}}${lb}`;
+  return `${s}${k}${v}${pth}${plD}`;
 };
+
+const json = (data, spaceCount = 1) => {
+  const tab = getTab(spaceCount);
+  const result = data.reduce((acc, [symb, key, value, pathName, plainData]) => {
+    const newValue = Array.isArray(value) ? json(value, spaceCount + 2) : value;
+    const node = `${lb}${tab}{${lb}${convertToJson([symb, key, newValue, pathName, plainData], spaceCount + 1)}${tab}},`;
+    return [...acc, node];
+  }, '').join('').slice(0, -1);
+
+  return `[${result}${lb}${getTab(spaceCount - 1)}]`;
+};
+
 export default json;
